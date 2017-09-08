4f8bdc636074c17f80b30780c16fca8cb5fc8e0d
Hani <msowe.feha@gmail.com>
2017-09-08 16:30:51 +0300

    connect/disconnect sockets

--- server/server.js
+++ server/server.js
@@ -1,19 +1,29 @@
 const path = require('path')
+const http = require('http')
 const express = require('express')
+const socketIO = require('socket.io')
 
 const public_path = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 var app = express()
+var server = http.createServer(app)
+var io = socketIO(server)
 
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-console.log('rllllllll');
-  res.send('Hello World!')
-  res.end()
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
+
+io.on('connection', (socket) => {
+  console.log('New user connected!');
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected user..');
+  })
+
 })
 
-app.listen(port, function () {
+server.listen(port, function () {
   console.log(`Chat app listening on port ${port}!`)
-
 })
